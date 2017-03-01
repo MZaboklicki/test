047c57f500f335e9e475d906e6710f9213bf218f
MZaboklicki <michal.dragon@gmail.com>
2017-03-01 23:56:39 +0100

    1

---
+++ console/console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms; //okienkowość, wymagania dodania dll w references
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ciekawostki
+
+            //Wykorzystanie okienka w aplikacji konsolowej
+           // MessageBox.Show("hello");
+        }
+    }
+}
