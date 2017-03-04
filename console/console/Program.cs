ced28870e5ee92ce9d542df3b6d4f201150b169d
MZaboklicki <michal.dragon@gmail.com>
2017-03-04 18:16:20 +0100

    d

--- console/console/Program.cs
+++ console/console/Program.cs
@@ -7,10 +7,34 @@ using System.Threading.Tasks;
 
 namespace console
 {
+    class Klasa
+    {
+        public delegate void del(string wiadomosc);
+        public del zwykly_del;
+        public event del zdarzenie_del;
+        public void Metoda_delegacja()
+        {
+            if (zwykly_del != null)
+                zwykly_del("Zwykły delegate...");
+            if (zdarzenie_del != null)
+                zdarzenie_del("Event delegate...");
+        }
+    }
     class Program
     {
+        static private void objekt_metodazakonczona(string wiadomosc)
+        {
+            Console.WriteLine(wiadomosc);
+        }
         static void Main(string[] args)
         {
+
+            ///delegate, events
+            Klasa obj = new Klasa();
+            //subskrypcje
+            obj.zwykly_del = objekt_metodazakonczona;
+            obj.zdarzenie_del += objekt_metodazakonczona;
+            obj.Metoda_delegacja();
 
             ///zmienne var i dynamic
             //var zm1 = 1L;
