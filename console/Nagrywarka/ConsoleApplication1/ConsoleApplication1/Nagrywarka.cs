1cd79aab781ed42168fac8ed5bb20c49625121af
MZaboklicki <michal.dragon@gmail.com>
2017-03-17 16:47:34 +0100

    N

---
+++ console/Nagrywarka/ConsoleApplication1/ConsoleApplication1/Nagrywarka.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Nagrywarka
+    {
+        private enum miejsceNagrywania { DVD,HDD };
+        private enum stan { Wyłączone, Zatrzymanie, Nagrywanie, Odtwarzanie};
+
+        private int aktualnyStan = 0;
+        private int aktualneMiejsce = 0;
+
+        public int Stan
+        {
+            get { return aktualnyStan; }
+        }
+        public int MiejsceNagrywania
+        {
+            get { return aktualneMiejsce; }
+            set { aktualneMiejsce = value; }
+        }
+        void Włącz()
+        {
+            if (aktualnyStan == 0)
+                aktualnyStan = (int)stan.Zatrzymanie;
+            else
+                Console.WriteLine("Nagrywarak jest już włączona");
+        }
+        void Odtwarzaj()
+        {
+            if (aktualnyStan == 1)
+                aktualnyStan = (int)stan.Odtwarzanie;
+            else
+                Console.WriteLine("Nie można w tej chwili nic nagrać");
+        }
+        void Nagrywaj()
+        {
+            if (aktualnyStan == 1)
+                aktualnyStan = (int)stan.Nagrywanie;
+            else
+                Console.WriteLine("Nie można w tej chwili nic odtworzyc");
+        }
+        void Zatrzymaj()
+        {
+            if (aktualnyStan == 2 || aktualnyStan==3)
+                aktualnyStan = (int)stan.Zatrzymanie;
+            else
+                Console.WriteLine("Nagrywarka zatrzymana, lub wyłączona");
+        }
+        public void AktualnyStan()
+        {
+            Console.WriteLine("Aktualny stan: "+ (stan)aktualnyStan);
+        }
+    
+    public void AktualneMiejsce()
+    {
+        Console.WriteLine("Aktualne miejsce: " + (miejsceNagrywania)aktualneMiejsce);
+    }
+}
+}
---
+++ console/Nagrywarka/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nagrywarka nag = new Nagrywarka();
+           
+            Console.WriteLine(nag.MiejsceNagrywania);
+            Console.ReadKey();
+        }
+    }
+}
